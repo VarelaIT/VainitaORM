44afbb469e3fad21fe0846296cf8d1e3d2088868
Ismael Varela <info@varelait.com>
2024-03-19 12:00:27 -0400

    readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# VainitaORM 
+
+A Hibernate implementation.
---
+++ src/main/java/JDBC/User.java
@@ -0,0 +1,7 @@
+package JDBC;
+
+public class User {
+   public final String name = "vainita";
+   public final String password = "vainita";
+   public final String url = "jdbc:postgresql://localhost:5432/vainitaorm";
+}
